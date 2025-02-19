466d4d0371cfd8d08814bd02680a78a704da0721
ThiagoEXE <your-email-addressthiagob.oliveira@ucsal.edu.br>
2025-02-18 21:01:49 -0300

    Adição de migrations e filtragem de imagens!

--- galeria/views.py
+++ galeria/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from galeria.models import Fotografia
 
 def index(request):
-    fotografias = Fotografia.objects.all()#orm do django
+    fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada=True)#orm do django
     return render(request, 'galeria/index.html', {"cards": fotografias})
 
 def imagem(request, foto_id):
